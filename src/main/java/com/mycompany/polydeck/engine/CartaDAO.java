4a9b9f8ae1d747413225a19198fbfa6222a9d2d1
Enrique3637 <egarcia22.0812@gmail.com>
2026-03-13 16:59:20 +0100

    Delete Card by id

--- src/main/java/com/mycompany/polydeck/engine/CartaDAO.java
+++ src/main/java/com/mycompany/polydeck/engine/CartaDAO.java
@@ -4,10 +4,46 @@
  */
 package com.mycompany.polydeck.engine;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.mycompany.polydeck.engine.model.Carta;
+
 /**
  *
  * @author alumnet
  */
 public class CartaDAO {
-    
+
+    public static void eliminarCartaPorId(long id) {
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("db/polydeck.odb");
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            // 1. Recuperar la carta (debe estar MANAGED)
+            Carta carta = em.find(Carta.class, id);
+
+            // 2. Si existe, eliminarla
+            if (carta != null) {
+                em.remove(carta);
+            }
+
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
 }
