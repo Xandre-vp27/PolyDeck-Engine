e86201fda33c28988084fff04f69e26e52775918
yiyi <yiyiliu84@gmail.com>
2026-03-13 16:31:14 +0100

    Cambio de orden de los imports

--- src/main/java/com/mycompany/polydeck/engine/PolyDeckEngine.java
+++ src/main/java/com/mycompany/polydeck/engine/PolyDeckEngine.java
@@ -4,14 +4,15 @@
 
 package com.mycompany.polydeck.engine;
 
-import com.mycompany.polydeck.engine.model.Carta;
-import com.mycompany.polydeck.engine.service.ImportadorCartes;
-import java.io.File;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+
+import com.mycompany.polydeck.engine.model.Carta;
+import com.mycompany.polydeck.engine.service.ImportadorCartes;
 
 /**
  *
@@ -36,7 +37,7 @@ public class PolyDeckEngine {
                 System.out.println("-> Esquema verge detectat. La base de dades no conté dades.");
             }
 
-            // Lógica de importación segura
+            // Importación de cartas 
             if (totalCartes == 0) {
                 System.out.println("-> Important cartes des de 'cartes.txt'...");
                 ImportadorCartes.importar("cartes.txt", em);
@@ -44,13 +45,13 @@ public class PolyDeckEngine {
                 System.out.println("-> Base de dades carregada. Total cartes: " + totalCartes);
             }
 
-            // Listado polimórfico [cite: 147, 150-151]
+            // Listado polimórfico 
             System.out.println("\n--- LLISTAT DE CARTES (Polimorfisme) ---");
             TypedQuery<Carta> query = em.createQuery("SELECT c FROM Carta c", Carta.class);
             List<Carta> llistaCartes = query.getResultList();
 
             for (Carta c : llistaCartes) {
-                System.out.println("- [" + c.getClass().getSimpleName() + "] " + c.getNom());
+                System.out.println("- "+ c.getId() +" [" + c.getClass().getSimpleName() + "] " + c.getNom());
             }
 
         } catch (Exception e) {
